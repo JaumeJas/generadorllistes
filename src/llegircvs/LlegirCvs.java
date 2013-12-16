bdbfafa25c55993e23f40fb73d466b9e9f443520
jaspreet <jsingh@infobosccoma.net>
2013-12-16 17:17:07 +0100

    commit fet per jas per el projecte v1

---
+++ src/llegircvs/LlegirCvs.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package llegircvs;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author MrSingh
+ */
+public class LlegirCvs {
+public static void main(String[] args) {
+		LlegirCvs l = new LlegirCvs();
+		l.llegirCvs();
+
+	}
+	
+	public void llegirCvs() { //el de la _ es el fitxer amb poques dades, el que te un . es el fitxer gran.
+		String csvFileToRead = "assig_matriculats.csv";
+		BufferedReader br = null;
+		String linia = "";
+		
+		// "^(\\d+)," -->funciona
+		String splitBy = "^(\\d+),";	
+		// "^(\"[a-zA-Zìíòóñàáèéùú]+\\s?[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]+[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]*[a-zA-Zìíòóñàáèéùú?]+)" : mostra tot el nom complert amb el , el mig
+		//" ^(\"[a-zA-Zìíòóñàáèéùú]+\\s?[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]+[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]*[a-zA-Zìíòóñàáèéùú?]+)" : versio 2, seguix mostrant el nom complert.
+		
+		Pattern patro_nomCognom = Pattern.compile("([a-zA-Zìíòóñàáèéùú]+\\s?[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]+[a-zA-Zìíòóñàáèéùú]+[\\s|,|\\s]*[a-zA-Zìíòóñàáèéùú?]+)");
+		Pattern patro_Classe = Pattern.compile("([1|2]?Batx[A|B|C]?)");
+		//peta falten els que son per exemple GG, 44
+		Pattern patro_materies = Pattern.compile("([\\d|A-Z[\\d|A-Z]+]{3},?)+");	//(([\\d*[A-Z]]){3},?)+	([\\d|A-Z[\\d|A-Z]+]{3},?)+
+		Matcher matcher_NomCognom= null;
+		Matcher matcher_classe = null;
+		Matcher matcher_materies = null;
+		try {
+			
+			br = new BufferedReader(new FileReader(csvFileToRead));
+			while ((linia = br.readLine()) != null) {
+				
+				String[] tLinia = linia.split(splitBy); // elimina el numero+, del començament de la linia; split busca un numero(10,100,1000) seguit de una ,
+				
+				for (int i = 0; i < tLinia.length; i++) {
+					matcher_NomCognom = patro_nomCognom.matcher(tLinia[i]);
+					matcher_classe = patro_Classe.matcher(tLinia[i]);
+					matcher_materies = patro_materies.matcher(tLinia[i]);
+					if (matcher_NomCognom.find()){	//treu el nom
+						if (matcher_classe.find()){	//treu el curs
+							if (matcher_materies.find()){ //treu tot el conjunt de materies
+							
+								extreureLlistatMateries(matcher_materies.group()); //extreu el llista de totes les materies
+							System.out.flush();
+							
+						//	System.out.println(com +" "+matcher_NomCognom.group() + " <-> " + matcher_classe.group()+ " <-> "+matcher_materies.group() );
+							
+							}
+						}
+					}
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally { //tanca el fitxer
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("\nFi del fitxer");
+	}
+
+	private void extreureLlistatMateries(String group) {
+		String ss = group;
+		System.out.println(ss);
+		
+	}
+    
+}
