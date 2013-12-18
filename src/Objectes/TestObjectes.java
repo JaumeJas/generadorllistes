304e682cdcd2f267f13c0f1c5e2b5651ec83688a
jsoler <jsoler@localhost>
2013-12-18 18:11:59 +0100

    Objectes

--- src/Objectes/TestObjectes.java
+++ src/Objectes/TestObjectes.java
@@ -1,5 +1,21 @@
 package Objectes;
 
 public class TestObjectes {
+    //Creem objecte Alumne
+    //Crear objecte materia
+    //Passar-hi Alumnes
+    public static void main(String[] args){
+        
+    
+    //Veure si ordenen correctament -Quin tipus de llista?
+    Alumne[] alumnes = {  new Alumne("Jaume","Soler","2DAM"),
+                        new Alumne("Andreu","Solà","2DAM"),
+                        new Alumne("Maria","Solà","2DAM"),
+                        new Alumne("Jaume","Andreu","2DAM"),
+                        new Alumne("Jaume","Mir","2DAM")};
+    
+    System.out.println(alumnes);
+    }
+    //ArrayList<Alumne> llista1 = new ArrayList<Alumne>();
     
 }
